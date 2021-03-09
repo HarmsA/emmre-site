234c8b262938d1da4bad51ddf7eef70211d002d4
Tony Klodd <tklodd@aimmedia.com>
2021-03-09 09:42:05 -0600

    Fixed the weird issue we were having with the console saying something about SuspiciousFileOperation.

--- writersdigest/settings.py
+++ writersdigest/settings.py
@@ -166,13 +166,13 @@ AWS_QUERYSTRING_AUTH = False
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
