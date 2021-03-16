418a6d1be9dfaf2c3675baf9811d591dd15c84ce
HarmsA <harms2a@gmail.com>
2021-03-16 13:29:05 -0500

    added sans font to admin pages

--- assets/conference/js/admin/conference.js
+++ assets/conference/js/admin/conference.js
@@ -1,6 +1,12 @@
 tinymce.init({
       selector: '#id_cancel_refund, #id_basic_conference, #id_all_access_conference',
       height: 600,
+      font_formats:"Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant" +
+          " garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier" +
+          " new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; " +
+          "Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; " +
+          "Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats;" +
+          "Open Sans=Open-Sans, sans-serif;",
       plugins: [
           'link image code',
           'insertdatetime media table paste imagetools wordcount'
--- assets/conference/js/admin/pitchslam.js
+++ assets/conference/js/admin/pitchslam.js
@@ -1,6 +1,12 @@
 tinymce.init({
       selector: '#id_description',
       height: 400,
+      font_formats:"Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant" +
+          " garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier" +
+          " new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; " +
+          "Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; " +
+          "Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats;" +
+          "Open Sans=Open-Sans, sans-serif;",
       plugins: 'link image code',
       relative_urls: false,
       remove_script_host: false,
