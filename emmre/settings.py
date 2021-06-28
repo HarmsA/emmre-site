701608eb7a7f9af67601307e9796afe0eb8f734b
adam harms <harms2a@gmail.com>
2021-06-28 16:59:21 -0500

    boto3 and storages added

--- emmre/settings.py
+++ emmre/settings.py
@@ -180,12 +180,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# AWS_ACCESS_KEY_ID = get_environment_variable('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = get_environment_variable('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = get_environment_variable('AWS_STORAGE_BUCKET_NAME')
-# AWS_DEFAULT_ACL = "public-read"
-# AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = get_environment_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_environment_variable('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_environment_variable('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = "public-read"
+AWS_QUERYSTRING_AUTH = False
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
